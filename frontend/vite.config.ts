8bae45a7cdcad2e88c27c95481964d9c246033bc
KiRA <sailinhtet.d@gmail.com>
2026-01-09 00:37:03 -0800

    update frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     allowedHosts: ["unmetalised-elna-nonstrategical.ngrok-free.dev"],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   plugins: [
     react({
