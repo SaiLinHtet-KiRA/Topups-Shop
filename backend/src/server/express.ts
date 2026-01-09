f671d0a47c8cee9932afd2d5fa24fd28a5545201
KiRA <sailinhtet.d@gmail.com>
2026-01-09 01:13:19 -0800

    update frontend

--- backend/src/server/express.ts
+++ backend/src/server/express.ts
@@ -15,25 +15,19 @@ export default class ExpressServer {
     this.app.set("trust proxy", 1);
     this.app.use(
       cors({
-        origin: [
-          "http://localhost:5173",
-          "https://424nhtsn-5173.asse.devtunnels.ms",
-          "https://unmetalised-elna-nonstrategical.ngrok-free.dev",
-          "https://topups-shop-1.onrender.com",
-        ],
+        origin: ["http://localhost:5173", "https://topups-shop-1.onrender.com"],
         credentials: true,
       })
     );
 
     this.app.use(
       cookie({
-        name: "eden_game_shop",
-        secret: COOKIE_SECRET,
-        maxAge: 2 * 24 * 60 * 1000,
-        keys: ["secretkey"],
+        name: "session",
+        keys: ["your-secret-key"],
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        secure: false,
-        sameSite: "none",
+        sameSite: "lax",
       })
     );
 
